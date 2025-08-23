0cdbda4a3a7ac79ed6c4304c59b3197d22fd9daa
Neo Tsay Yong <padricneo@gmail.com>
2025-08-24 01:51:39 +0800

    Parse dates/times with java.time and pretty-print deadlines/events

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,23 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Event extends Task {
     protected final String from;
     protected final String to;
 
+    private final LocalDateTime fromDt, toDt;
+    private final LocalDate fromDate, toDate;
+
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
-        this.to = to;
+        this.to   = to;
+
+        this.fromDt = Dates.tryParseDateTime(from);
+        this.toDt   = Dates.tryParseDateTime(to);
+
+        this.fromDate = (fromDt == null) ? Dates.tryParseDate(from) : null;
+        this.toDate   = (toDt   == null) ? Dates.tryParseDate(to)   : null;
     }
 
     public String getFrom() {
@@ -24,6 +36,12 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return super.toString() + " (from: " + from + " to: " + to + ")";
+        String fromDisp = (fromDt != null) ? Dates.format(fromDt)
+                         : (fromDate != null) ? Dates.format(fromDate)
+                         : from;
+        String toDisp = (toDt != null) ? Dates.format(toDt)
+                        : (toDate != null) ? Dates.format(toDate)
+                        : to;
+        return super.toString() + " (from: " + fromDisp + " to: " + toDisp + ")";
     }
 }

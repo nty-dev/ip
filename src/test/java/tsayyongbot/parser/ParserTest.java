77612bca70394b14a7df94b97f032309248156ca
Neo Tsay Yong <padricneo@gmail.com>
2025-09-11 18:45:40 +0800

    Extract constants and remove wildcard imports

--- src/test/java/tsayyongbot/parser/ParserTest.java
+++ src/test/java/tsayyongbot/parser/ParserTest.java
@@ -1,9 +1,11 @@
 package tsayyongbot.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import tsayyongbot.core.TsayYongBotException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
 
